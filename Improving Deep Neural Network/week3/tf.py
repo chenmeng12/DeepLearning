0935f6484eaffb6c8ca76d775d3c1e5c321fbc27
chen <1094955329@qq.com>
2019-06-21 17:46:30 +0800

    第三周作业，超参数调整，1 新增tf_utils文件

--- Improving Deep Neural Network/week3/tf.py
+++ Improving Deep Neural Network/week3/tf.py
@@ -3,4 +3,7 @@ import numpy as np
 import h5py
 import matplotlib.pyplot as plt
 import tensorflow as tf
-from te
+from tensorflow.python.framework import ops
+from tf_utils import *
+
+np.random.seed(1)

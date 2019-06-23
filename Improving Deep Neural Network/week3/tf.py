75c9b34442989db78d6168e529232f2462348260
chen <1094955329@qq.com>
2019-06-23 20:02:27 +0800

    删除py文件，新增练习作业

--- Improving Deep Neural Network/week3/tf.py
+++
@@ -1,9 +0,0 @@
-import math
-import numpy as np
-import h5py
-import matplotlib.pyplot as plt
-import tensorflow as tf
-from tensorflow.python.framework import ops
-from tf_utils import *
-
-np.random.seed(1)
